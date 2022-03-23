934c384e5e644a2d39ef42a26ffc198407acc487
zs-zhanglvxiu <p2wan@163.com>
2022-03-23 18:10:54 +0800

    引入测试模块

--- wanwan-demo/wanwan-unitest/src/test/java/MyTest.java
+++ wanwan-demo/wanwan-unitest/src/test/java/MyTest.java
@@ -1,3 +1,4 @@
+import com.google.common.base.CharMatcher;
 import org.junit.Test;
 
 /**
@@ -7,6 +8,6 @@ public class MyTest {
 
 	@Test
 	public void test1() {
-
+		boolean isText = !CharMatcher.anyOf("x×*").matchesAnyOf("2个字-100x10px");
 	}
 }
